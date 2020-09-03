cdd8308bbb0de882754f0ba1cf29ff5badc48c41
Lina Sachuk <sachuklina@gmail.com>
2020-09-02 23:24:49 -0600

    add delete apt

--- reactinterface/src/component/App.js
+++ reactinterface/src/component/App.js
@@ -4,7 +4,7 @@ import '../css/App.css';
 import AddAppointments from './AddAppointments';
 import SearchAppointments from './SearchAppointments';
 import ListAppointments from './ListAppointments';
-
+import { without } from 'lodash';
 
 class App extends Component {
 
@@ -15,7 +15,21 @@ class App extends Component {
       myAppointments: [],
       lastIndex: 0
     }
+
+    this.deleteAppointment = this.deleteAppointment.bind(this);
   }
+
+  deleteAppointment(apt) {
+    let tempApts = this.state.myAppointments;
+    tempApts = without(tempApts, apt);
+
+    this.setState({
+      myAppointments: tempApts
+    })
+  }
+
+
+
 
   componentDidMount() {
     fetch('./data.json')
@@ -52,7 +66,11 @@ class App extends Component {
               <div className="container">
                 <AddAppointments />
                 <SearchAppointments />
-                <ListAppointments appointments={this.state.myAppointments} />
+                <ListAppointments
+                  appointments={this.state.myAppointments}
+                  deleteAppointment={this.deleteAppointment}
+
+                />
               </div>
             </div>
           </div>
